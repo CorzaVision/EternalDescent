9acff03df0b33dd19f2c3ca52973d6538b3a7b25
CorzaVision <corzabarnes@gmail.com>
2025-08-11 14:25:08 +1000

    Claude gen and fix 2

--- EternalDescent/Intermediate/Build/Win64/UnrealEditor/Inc/EternalDescent/UHT/EternalDescent.init.gen.cpp
+++ EternalDescent/Intermediate/Build/Win64/UnrealEditor/Inc/EternalDescent/UHT/EternalDescent.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeEternalDescent_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x87B84724,
-				0x2A32A8BC,
+				0x99BD979D,
+				0xE337FB63,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_EternalDescent.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_EternalDescent.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_EternalDescent(Z_Construct_UPackage__Script_EternalDescent, TEXT("/Script/EternalDescent"), Z_Registration_Info_UPackage__Script_EternalDescent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x87B84724, 0x2A32A8BC));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_EternalDescent(Z_Construct_UPackage__Script_EternalDescent, TEXT("/Script/EternalDescent"), Z_Registration_Info_UPackage__Script_EternalDescent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x99BD979D, 0xE337FB63));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
